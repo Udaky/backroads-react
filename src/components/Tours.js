40efed89889ac899c3d533fa279e8cc70f1ce7fa
Udaky <rizkyfakhri227@gmail.com>
2023-11-29 12:47:17 +0700

    final

---
+++ src/components/Tours.js
@@ -0,0 +1,18 @@
+import Tour from "./Tour";
+import Title from "./Title";
+import { tours } from "../data";
+
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <Title title="feature" subTitle="tours" />
+      <div className="section-center featured-center">
+        {tours.map((tour) => {
+          return <Tour {...tour} key={tour.id} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
